914733c60fc8c9c4704cdca0d9d234f41676e140
Je Yun <alwaysblue21@gmail.com>
2023-07-08 13:37:42 -0400

    controllers

---
+++ controllers/lolChampionRoutes.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const LolChampion = require("../models/lolchampion")
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('Hello World!');
+    });
+
+// index
+router.get("/lolchampions", async (req, res) => {
+    const allLolChampions = await LolChampion.find({});
+    res.render("index.ejs", {lolChampions: allLolChampions})
+})
+
+// new 
+router.get("/lolchampions/new", (req, res) => {
+    res.render("new.ejs")
+})
+
+// delete
+router.delete("/lolchampions/:id", async (req, res) => {
+    await LolChampion.findByIdAndDelete(req.params.id)
+    res.redirect("/lolchampions")
+})
+// update
+router.put("/lolchampions/:id", async (req, res) => {
+    if (req.body.readyToBattle === "on") {
+        req.body.readyToBattle = true;
+    } else {
+        req.body.readyToBattle = false;
+    }
+    await LolChampion.findByIdAndUpdate(req.params.id, req.body);
+    res.redirect("/lolchampions");
+})
+
+// create 
+router.post("/lolchampions", async (req, res) => {
+    if (req.body.readyToBattle === "on" ) {
+        req.body.readyToBattle = true;
+    } else {
+        req.body.readyToBattle = false;
+    }
+    await LolChampion.create(req.body)
+    res.redirect("/lolchampions")
+})
+// edit
+router.get("/lolchampions/:id/edit", async (req, res) => {
+    const lolChampion = await LolChampion.findById(req.params.id)
+    res.render("edit.ejs", { lolChampion })
+})
+
+// show
+router.get("/lolchampions/:id", async (req, res) => {
+    const foundLolChampion = await LolChampion.findById(req.params.id)
+    res.render("show.ejs", { lolChampion: foundLolChampion })
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,7 +3,9 @@ require('dotenv').config(); // bring in our .env vars
 const express = require('express'); // web framework for node
 const morgan = require('morgan'); // logger for node
 const methodOverride = require('method-override'); // allows us to use PUT and DELETE methods
-const LolChampion = require("./models/lolchampion");
+// const LolChampion = require("./models/lolchampion");
+const lolChampionRouter = require("./controllers/lolChampionRoutes")
+
 // express application
 const app = express();
 
@@ -12,61 +14,61 @@ app.use(morgan('dev')); // logging
 app.use(methodOverride('_method')); // override with POST having ?_method=DELETE or ?_method=PUT
 app.use(express.static('public')); // serve static files from public folder
 app.use(express.urlencoded({ extended: false })); // whenever we send a form we need urlencoded
-
+app.use(lolChampionRouter)
 // Routes
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    });
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+//     });
 
-// index
-app.get("/lolchampions", async (req, res) => {
-    const allLolChampions = await LolChampion.find({});
-    res.render("index.ejs", {lolChampions: allLolChampions})
-})
+// // index
+// app.get("/lolchampions", async (req, res) => {
+//     const allLolChampions = await LolChampion.find({});
+//     res.render("index.ejs", {lolChampions: allLolChampions})
+// })
 
-// new 
-app.get("/lolchampions/new", (req, res) => {
-    res.render("new.ejs")
-})
+// // new 
+// app.get("/lolchampions/new", (req, res) => {
+//     res.render("new.ejs")
+// })
 
-// delete
-app.delete("/lolchampions/:id", async (req, res) => {
-    await LolChampion.findByIdAndDelete(req.params.id)
-    res.redirect("/lolchampions")
-})
-// update
-app.put("/lolchampions/:id", async (req, res) => {
-    if (req.body.readyToBattle === "on") {
-        req.body.readyToBattle = true;
-    } else {
-        req.body.readyToBattle = false;
-    }
-    await LolChampion.findByIdAndUpdate(req.params.id, req.body);
-    res.redirect("/lolchampions");
-})
+// // delete
+// app.delete("/lolchampions/:id", async (req, res) => {
+//     await LolChampion.findByIdAndDelete(req.params.id)
+//     res.redirect("/lolchampions")
+// })
+// // update
+// app.put("/lolchampions/:id", async (req, res) => {
+//     if (req.body.readyToBattle === "on") {
+//         req.body.readyToBattle = true;
+//     } else {
+//         req.body.readyToBattle = false;
+//     }
+//     await LolChampion.findByIdAndUpdate(req.params.id, req.body);
+//     res.redirect("/lolchampions");
+// })
 
-// create 
-app.post("/lolchampions", async (req, res) => {
-    if (req.body.readyToBattle === "on" ) {
-        req.body.readyToBattle = true;
-    } else {
-        req.body.readyToBattle = false;
-    }
-    await LolChampion.create(req.body)
-    res.redirect("/lolchampions")
-})
-// edit
-app.get("/lolchampions/:id/edit", async (req, res) => {
-    const lolChampion = await LolChampion.findById(req.params.id)
-    res.render("edit.ejs", { lolChampion })
-})
+// // create 
+// app.post("/lolchampions", async (req, res) => {
+//     if (req.body.readyToBattle === "on" ) {
+//         req.body.readyToBattle = true;
+//     } else {
+//         req.body.readyToBattle = false;
+//     }
+//     await LolChampion.create(req.body)
+//     res.redirect("/lolchampions")
+// })
+// // edit
+// app.get("/lolchampions/:id/edit", async (req, res) => {
+//     const lolChampion = await LolChampion.findById(req.params.id)
+//     res.render("edit.ejs", { lolChampion })
+// })
 
-// show
-app.get("/lolchampions/:id", async (req, res) => {
-    const foundLolChampion = await LolChampion.findById(req.params.id)
-    res.render("show.ejs", { lolChampion: foundLolChampion })
-})
+// // show
+// app.get("/lolchampions/:id", async (req, res) => {
+//     const foundLolChampion = await LolChampion.findById(req.params.id)
+//     res.render("show.ejs", { lolChampion: foundLolChampion })
+// })
 
 
 
